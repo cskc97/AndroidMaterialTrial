c68532c736f0789c194f4bb8c02d5f479f297ae9
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-02-08 16:50:49 +0800

    Testing Libraries

--- app/src/main/java/apps/everythingforward/com/androidmaterialtrial/MainActivity.java
+++ app/src/main/java/apps/everythingforward/com/androidmaterialtrial/MainActivity.java
@@ -2,8 +2,13 @@ package apps.everythingforward.com.androidmaterialtrial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.gc.materialdesign.views.Button;
+import com.skyfishjy.library.RippleBackground;
+
+import tyrantgit.explosionfield.ExplosionField;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +19,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final RippleBackground rippleBackground=(RippleBackground)findViewById(R.id.content);
+        ImageView imageView=(ImageView)findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rippleBackground.startRippleAnimation();
+            }
+        });
         button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               if(rippleBackground.isRippleAnimationRunning()) {
+                   rippleBackground.stopRippleAnimation();
+               }
+                ExplosionField explosionField=new ExplosionField(getApplicationContext());
+                explosionField.explode(button);
+
+            }
+        });
+
+
 
 
     }
